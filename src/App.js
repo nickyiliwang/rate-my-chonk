13265c4fc888a693c9e1c1fd2f7f2f1756b63874
Nick Wang <nickyiliwang@gmail.com>
2019-11-19 10:03:23 -0500

    basic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Rate My Chonk</h1>
+        
+
       </header>
     </div>
   );
---
+++ src/components/SwipeCard.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./SwipeCard.css";
+
+export default class SwipeCard extends Component {
+  render() {
+    return (
+      <div id="board">
+        <div className="card"></div>
+      </div>
+    );
+  }
+}
