cd3818ea8f0c5c75c78f919e026a10e1fbc75b8d
Nick Wang <nickyiliwang@gmail.com>
2019-11-21 13:42:21 -0500

    more stuff

---
+++ src/components/backend/UploadMyChonks.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UploadMyChonks extends Component {
+    render() {
+        return (
+            <div>
+                <h2>UploadMyChonks</h2>
+            </div>
+        )
+    }
+}
